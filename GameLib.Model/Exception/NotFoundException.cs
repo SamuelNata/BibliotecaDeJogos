b71728f21adfe94bccd06239d67fe7ebea9de6be
samuelnata <nataborges9@gmail.com>
2021-03-28 12:40:53 -0300

    Adding 2 exceptions to the list of friently exceptions

--- GameLib.Model/Exception/NotFoundException.cs
+++ GameLib.Model/Exception/NotFoundException.cs
@@ -3,7 +3,7 @@ namespace GameLib.Model.Exception
     /// <summary>
     /// Exception that, when throwed, will return a 404 to the client
     /// </summary>
-    public class NotFoundException : System.Exception
+    public class NotFoundException : UserFriendlyException
     {
         public NotFoundException(string message) : base(message){
 

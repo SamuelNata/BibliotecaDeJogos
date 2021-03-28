57cfd65571d2dfde01738a04faa481ae6142d1fb
samuelnata <nataborges9@gmail.com>
2021-03-28 12:42:08 -0300

    Configure logging and add logging on exceptions hanfler filter

--- GameLib.API/Program.cs
+++ GameLib.API/Program.cs
@@ -18,6 +18,11 @@ namespace GameLib.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
